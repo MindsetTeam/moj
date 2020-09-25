a2f1f3dcd774c72a9520cf38a9d3ed80e90dfc83
seuminh <seuminh@yahoo.com>
2020-09-25 10:40:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-success">Hello My Friend</p>
+    </div>
+  );
+}
+
+export default App;
