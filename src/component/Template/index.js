c19f41d449dbe33d2c6cb1b5450085eb77084c80
seuminh <seuminh@yahoo.com>
2020-11-04 09:31:36 +0700

    add template folder

---
+++ src/component/Template/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./index.module.css";
+
+export class index extends Component {
+  render() {
+    return (
+      <div style={styles.templateContainer}>
+        <p>Template</p>
+      </div>
+    );
+  }
+}
+
+export default index;
