d3172e4cf57ee96b37911b10748b55069506752f
seuminh <seuminh@yahoo.com>
2020-12-15 09:35:29 +0700

    update

---
+++ q
@@ -0,0 +1,33 @@
+[33mb4249ac[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m add tab in contact page
+[33m5b01657[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m updated
+[33m76761df[m add 404
+[33ma7a3b22[m update
+[33mc19f41d[m add template folder
+[33m31f7134[m add material UI
+[33mdc941cd[m fixed related news css
+[33m999f103[m update gallery page modal
+[33me712dcc[m Update law documents page
+[33me5e5925[m update
+[33mdb34ea8[m update
+[33m7acebcc[m add context api
+[33m2a06b5c[m update gallery
+[33ma9ed4ea[m update
+[33m7d0ce7d[m fixed
+[33m93cd682[m fix
+[33mc9f7f08[m add gallery feature
+[33m64f6848[m update
+[33me14b21e[m fix gallery page
+[33m955f183[m news doen
+[33ma192067[m update
+[33mfbca9e4[m update
+[33m5ad03a7[m udpate homepage
+[33m2a1267b[m update
+[33m1deacc9[m add modal in gallery page
+[33mc1b4295[m complete design Gallery Page
+[33mb316d09[m design Gallery Page 2 (in progress)
+[33m061f391[m desigin Gallery Page (in progress)
+[33me52108a[m lazy loading components split module code
+[33mea9fc69[m complete MinistryBiography, PreviousMinisters and WelcomeMessage Page
+[33m75df157[m add css module in homepage and contact page
+[33mee61719[m complete Homepage & Contact Page
+[33ma2f1f3d[m first commit
--- src/component/CriminalRecord/cv.js
+++ src/component/CriminalRecord/cv.js
@@ -6,127 +6,156 @@ import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-   root: {
-      "& > *": {
-         margin: theme.spacing(1),
-         width: "25ch",
-      },
-   },
-   input: {
-      fontFamily: "hanuman",
-   },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  input: {
+    fontFamily: "hanuman",
+  },
 }));
 
 const genders = [
-   {
-      value: "male",
-      label: "ប្រុស",
-   },
-   {
-      value: "female",
-      label: "ស្រី",
-   },
+  {
+    value: "male",
+    label: "ប្រុស",
+  },
+  {
+    value: "female",
+    label: "ស្រី",
+  },
 ];
 
 export default function () {
-   const classes = useStyles();
-   const [gender, setGender] = React.useState("");
+  const classes = useStyles();
+  const [gender, setGender] = React.useState("");
 
-   const handleChange = (event) => {
-      setGender(event.target.value);
-   };
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
 
-   return (
-      <div className={styles.templateContainer}>
-         <form className={classes.root} noValidate autoComplete="off">
-            <TextField
-               id="standard-basic"
-               label="នាមត្រកូល និង​ នាមខ្លួន"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "300px" }}
-            />
-            <TextField
-               id="standard-basic"
-               label="អក្សរឡាតាំង"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "300px" }}
-            />
-            <TextField
-               id="standard-select-currency"
-               select
-               label="ភេទ"
-               value={gender}
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               onChange={handleChange}
-               style={{ width: "100px" }}
-               //    helperText="Please select your currency"
-            >
-               {genders.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                     {option.label}
-                  </MenuItem>
-               ))}
-            </TextField>
-            <TextField
-               id="standard-basic"
-               label="សញ្ជាតិដើម"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "130px" }}
-            />
-            <TextField
-               id="standard-basic"
-               label="សញ្ជាតិបច្ចុប្បន្ន"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "130px" }}
-            />
-            <TextField
-               id="standard-basic"
-               label="ថ្ងៃ ខែ​ ឆ្នាំកំណើត"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "500px" }}
-            />
-            <TextField
-               id="standard-basic"
-               label="ទីកន្លែងកំណើត"
-               InputProps={{
-                  className: classes.input,
-               }}
-               InputLabelProps={{
-                  className: classes.input,
-               }}
-               style={{ width: "500px" }}
-            />
-         </form>
-      </div>
-   );
+  return (
+    <div className={styles.templateContainer}>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="standard-basic"
+          label="នាមត្រកូល និង​ នាមខ្លួន"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          style={{ width: "450px" }}
+        />
+        <TextField
+          id="standard-basic"
+          label="អក្សរឡាតាំង"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          style={{ width: "450px" }}
+        />
+        <TextField
+          id="standard-select-currency"
+          select
+          label="ភេទ"
+          value={gender}
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          onChange={handleChange}
+          style={{ width: "100px" }}
+          //    helperText="Please select your currency"
+        >
+          {genders.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="standard-basic"
+          label="សញ្ជាតិដើម"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          label="សញ្ជាតិបច្ចុប្បន្ន"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+        />
+
+        <TextField
+          id="date"
+          label="ថ្ងៃ ខែ​ ឆ្នាំកំណើត"
+          type="date"
+          defaultValue="1980-01-01"
+          format="dd/MM/yyyy"
+          InputProps={{
+            className: classes.input,
+            shrink: true,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+        />
+        <TextField
+          id="standard-basic"
+          label="មុខរបរ"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          multiline
+          rowsMax={4}
+        />
+        <TextField
+          id="standard-basic"
+          label="ទីកន្លែងកំណើត"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          multiline
+          rowsMax={4}
+          style={{ width: "100%" }}
+        />
+        <TextField
+          id="standard-basic"
+          label="អាស័យដ្ឋានបច្ចុប្បន្ន"
+          InputProps={{
+            className: classes.input,
+          }}
+          InputLabelProps={{
+            className: classes.input,
+          }}
+          multiline
+          rowsMax={4}
+          style={{ width: "100%" }}
+        />
+      </form>
+    </div>
+  );
 }
